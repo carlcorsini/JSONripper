2e795fc326585738b3a94fafa0838686997c402a
Carl Corsini <carlcorsini@Carls-MacBook-Air.local>
2018-08-17 13:35:47 -0700

    *args

--- ripper.py
+++ ripper.py
@@ -1,6 +1,22 @@
-def key_to_value(**kwargs):
-  for key, value in kwargs.items():
-    print("{0} = {1}".format(key, value))
+first_names = {"1": "gary", "2": "larry",}
+last_names = {"1": "johnson", "2": "swanson"}
 
 
-print(key_to_value({"hey": "3"}))
+
+class KeyToValue():
+  def __init__(self, **kwargs):
+    self.dict = {}
+    for key, value in kwargs.items():
+      self.dict[key] = value
+
+  def json(self):
+    rip = {}
+    for key, value in self.dict.items():
+      rip[key] = value
+    print(rip)
+
+
+new = KeyToValue(**first_names)
+
+
+new.json()
